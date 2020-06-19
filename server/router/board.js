98d826d0755b0fdc747b9f2bc8b4dae2ff032170
limaries30 <limaries30@gmail.com>
2020-06-19 12:29:55 +0900

    update_board

--- server/router/board.js
+++ server/router/board.js
@@ -90,8 +90,30 @@ router.get('/board/write', function(req, res, next) {
     res.render('write');
 });
 
+
+//게시글 업데이트
+router.get('/update_post/:id',function(req,res){
+  let id = req.params.id
+  Board.find({_id:id},function(req,response)
+  {
+    console.log("find board",response[0])
+    res.render('board_update',response[0])
+  })
+
+
+})
+
+
+router.post('/api/users/update_board',function(req,res)
+{
+
+  Board.findOneAndUpdate({_id:req.body._id},{$set:req.body},(err,doc)=>{
+    res.redirect('/api/users/myspace')
+})
+})
+
 //글쓰기 post
-router.post('/board', function (req, res) {
+router.post('/api/users/board', function (req, res) {
     
     let x_auth = req.cookies.x_auth
     let login = Object.keys(req.cookies).includes('x_auth')?true:false

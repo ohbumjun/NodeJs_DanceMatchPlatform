9294fe79a81aa35cc23361a73b58ac0eef41a8d4
limaries30 <limaries30@gmail.com>
2020-08-22 21:08:11 +0900

    change2ncloud

--- server/models/Board.js
+++ server/models/Board.js
@@ -1,27 +1,25 @@
-var mongoose = require('mongoose');
-const {User}   = require('./User')
-const {Comment}= require('./Comment')
-var Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const { User } = require("./User");
+const { Comment } = require("./Comment");
 
+const { Schema } = mongoose;
 
-
-var boardSchema = new Schema({
-    title: String,
-    contents: String,
-    place:String,
-    author:User.schema,
-    video: String,
-    time:String,
-    people : Number,
-    current_people :Number,
-    board_date: {type: Date, default: Date.now()},
-    comments: [Comment.schema],
-    members:[User.schema],
-    tmp_members:[User.schema]
+const boardSchema = new Schema({
+  title: String,
+  contents: String,
+  place: String,
+  author: User.schema,
+  video: String,
+  time: String,
+  people: Number,
+  current_people: Number,
+  board_date: { type: Date, default: Date.now() },
+  comments: [Comment.schema],
+  members: [User.schema],
+  tmp_members: [User.schema],
 });
 
+// recursive한거 때문에 콜스택 에러 발생??
+// boardSchema.plugin(require('mongoose-beautiful-unique-validation'));
 
-//recursive한거 때문에 콜스택 에러 발생??
-// boardSchema.plugin(require('mongoose-beautiful-unique-validation')); 
-
-module.exports = mongoose.model('board', boardSchema);
+module.exports = mongoose.model("board", boardSchema);
